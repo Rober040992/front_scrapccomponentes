3d60951c6a09eff4c5f96a45e91e6a25df122a5a
RoberGF <rgfrasta@gmail.com>
2025-07-23 19:22:52 +0200

    First commit - basic scaffold

---
+++ src/app/productos/[slug]/page.tsx
@@ -0,0 +1,5 @@
+import ProductDetailPage from "@/components/ProductDetalPage"
+
+export default function ProductSlugPage() {
+  return <ProductDetailPage />
+}
---
+++ src/components/ProductDetalPage.tsx
@@ -0,0 +1,7 @@
+export default function ProductDetailPage() {
+    return(
+        <div>
+            que dise desde detailPage
+        </div>
+    )
+}
